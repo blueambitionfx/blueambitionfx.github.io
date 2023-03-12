1706982bb8dd58ba5230c44bf027df9d5a765c04
Alberto Moreno <albertomorenoayala8@gmail.com>
2023-03-12 17:05:54 +0100

    Primera importación página

---
+++ Sitio en construcción/README.md
@@ -0,0 +1,13 @@
+Coming Soon
+===========
+Coming Soon es una página simple, animada y responsiva de "pagina en construcción".
+
+- HTML y CSS
+- Soporte para múltiples navegadores (hasta la fecha de los principales navegadores)
+- Diseño responsivo
+
+[Demo](https://eltictacdicta.github.io/coming-soon/)
+
+Es una traducción/fork de [el proyecto coming soon de YC](https://github.com/YC/coming-soon)
+
+<img src="captura-de-pantalla.jpg" width="960" height="540" />
---
+++ funciones.js
@@ -0,0 +1,101 @@
+
+//DETECTA SI EL DISPOSITIVO TIENE ACTIVADO EL MODO OSCURO O CLARO
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (event) => {
+  if (event.matches) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+    dark = true;
+  } else {
+    document.documentElement.setAttribute('data-theme', 'light');
+    dark=false;
+  }
+});
+
+
+// BOTÓN TELEGRAM
+window.onload = function botonTelegram() {
+  setTimeout(function botonTelegram () {
+      document.getElementById("TELEGRAM").style.display = "flex";
+  }, 10000);
+
+  setTimeout(function botonTelegram () {
+    document.getElementById("exit").style.display = "block";
+}, 14000);
+}
+
+// Exit botón telegram
+
+function exit(){
+  document.getElementById("TELEGRAM").style.display="none";
+  document.getElementById("exit").style.display="none";
+}
+
+let dark = false;
+
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    var d = new Date();
+    var n = d.getFullYear();
+    document.getElementById("year").innerHTML = n;
+});
+
+
+  
+  function darkMode() {
+
+    if(dark === false){
+        document.getElementById("sol").style.display="block";
+        document.getElementById("luna").style.display="none";
+        document.getElementById("header").style.setProperty("background-color","black","important");
+        document.querySelector("header").style.setProperty("background-color","black","important");
+        document.getElementById("body").style.backgroundColor = "black";
+        document.getElementById("body").style.color = "white";
+        document.getElementsByClassName("nav-link")[1].style.color = "white";
+        document.getElementsByClassName("nav-link")[2].style.color = "white";
+        document.getElementsByClassName("nav-link")[3].style.color = "white";
+        document.getElementsByClassName("nav-link")[4].style.color = "white";
+
+        document.getElementById("realidadVirtual2").style.display="block";
+        document.getElementById("realidadVirtual").style.display="none";
+
+
+        document.getElementsByClassName("text-muted")[0].style.setProperty("color", "white" , "important");
+        document.getElementsByClassName("text-muted")[1].style.setProperty("color", "white", "important");
+        document.getElementsByClassName("text-muted")[2].style.setProperty("color", "white", "important");
+        document.getElementsByClassName("text-muted")[3].style.setProperty("color", "white", "important");
+        document.getElementsByClassName("text-muted")[4].style.setProperty("color", "white", "important");
+        document.getElementsByClassName("text-muted")[5].style.setProperty("color", "white", "important");
+
+        dark = true;
+    }
+    
+    else{
+        document.getElementById("sol").style.display="none";
+        document.getElementById("luna").style.display="block";
+        document.getElementById("header").style.setProperty("background-color","white","important");
+        document.querySelector("header").style.setProperty("background-color","white","important");
+        document.getElementById("body").style.backgroundColor = "white";
+        document.getElementById("body").style.color = "black";
+
+        document.getElementsByClassName("nav-link")[1].style.setProperty("color","inherit","important");
+        document.getElementsByClassName("nav-link")[2].style.setProperty("color","inherit","important");
+        document.getElementsByClassName("nav-link")[3].style.setProperty("color","inherit","important");
+        document.getElementsByClassName("nav-link")[4].style.setProperty("color","inherit","important");
+
+        document.getElementsByClassName("text-muted")[0].style.setProperty("color", "inherit","important");
+        document.getElementsByClassName("text-muted")[1].style.setProperty("color", "inherit","important");
+        document.getElementsByClassName("text-muted")[2].style.setProperty("color", "inherit","important");
+        document.getElementsByClassName("text-muted")[3].style.setProperty("color", "inherit","important");
+        document.getElementsByClassName("text-muted")[4].style.setProperty("color", "inherit","important");
+        document.getElementsByClassName("text-muted")[5].style.setProperty("color", "inherit","important");
+
+        document.getElementById("realidadVirtual2").style.display="none";
+        document.getElementById("realidadVirtual").style.display="block";
+        
+        dark = false;
+    } 
+  }
+
+
+
+
+ 
